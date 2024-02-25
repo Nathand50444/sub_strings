0a7c404157f145f4c2c49b528531588f3dc3d86e
NathanD <nathand50444@gmail.com>
2024-02-25 22:27:07 +0000

    Solution with explanations added to each line.

--- substrings.rb
+++ substrings.rb
@@ -1,13 +1,13 @@
 def substrings(string, dictionary)
-    result = Hash.new(0)
-    string.downcase!
+    result = Hash.new(0) # We create a new, empty hash called 'result'
+    string.downcase! # The starting string is converted to lowercase.
     
-    dictionary.each do |substring|
-        count = string.scan(substring.downcase).length
-        result[substring] += count if count > 0
+    dictionary.each do |substring| # We iterate through each word in the 'dictionary'.
+        count = string.scan(substring.downcase).length # Here we scan for the word in the string and return the number of times it is found.
+        result[substring] += count if count > 0 # if a word is found more than zero times then it is added to the hash AND th enumber of instances is concatenated onto it.
     end
 
-    result
+    result # The result is printed.
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
